7f742df578dbfbf3ce9d20ac03aa726587d3ab35
qMaDMaNp <medifman@gmail.com>
2023-10-02 18:14:55 -0400

    Add navigation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,11 +2,8 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function HomePage() {
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-      <main>
+    <>
         <h1 className='head-text'>Home</h1>
-      </main>
-    </div>
+    </>
   )
 }
